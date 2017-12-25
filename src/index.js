19d0f384c1eae1a2d99098b5255859ac11c9ab1e
Shwetank30 <shwetank.kumar.cer13@itbhu.ac.in>
2017-12-26 04:11:54 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import store from './stores'
+import { Provider } from 'react-redux'
+import Intro from './components/Intro'
+
+
+const app = (
+	<Provider store={store.configure(null)}>
+		<Intro />
+	</Provider>
+)
+
+
+ReactDOM.render(app, document.getElementById('root'))
